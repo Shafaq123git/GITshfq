c3aee8b2a43fdfc1ea6c1da96a1e52bab577b656
Shafaq123git <shafaqiqbal3636@gmail.com>
2023-03-02 14:24:46 +0500

    create sum and mul function

--- 1.js
+++ 1.js
@@ -1,2 +1,14 @@
-console.log("sum")
-console.log("multiply")
+function sum(){
+    let a=4;
+    let b=4;
+    let c=a+b;
+    console.log(c)
+}
+sum();
+function mul(){
+    let a=4;
+    let b=4;
+    let c=a*b;
+    console.log(c)
+}
+mul();
